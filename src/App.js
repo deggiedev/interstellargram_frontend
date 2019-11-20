42b0460b3494e92a4a5b751303958a423362999b
Jack de Grave <jack.degrave@outlook.com>
2019-11-20 19:34:34 +0000

    NavBar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import MainContainer from './containers/MainContainer';
+import NavBar from './containers/NavBar';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header>
-        InterstellarGram
-      </header>
+      <NavBar />
       <MainContainer />
     </div>
   );
---
+++ src/containers/NavBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class NavBar extends React.Component {
+ 
+    render() {
+      return (
+          <>
+          <a href>Home</a>
+          </>
+        );
+      }
+    }
+  
+export default NavBar;
