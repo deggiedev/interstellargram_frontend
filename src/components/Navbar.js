a6a3289e2b27b4cd741c7075f7b2ef86006ef974
Jack de Grave <jack.degrave@outlook.com>
2019-11-27 16:49:38 +0000

    navbar react routes partially complete

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,42 @@
 import React from 'react'
 import UserForm from './UserForm'
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link } from "react-router-dom";
+
 
 const Navbar = ({ user, signUp, logIn, logOut }) => {
     return (
-        <nav>
-            {
-                user && !user.error ? <div><button onClick={logOut}>Log out</button></div> :
-                    <>
-                        <UserForm submit={signUp} header={'Sign up'} />
-                        or
-                        <UserForm submit={logIn} header={'Log in'} />
-                    </>
-            }
-        </nav>
+        <Router>        
+            <nav>
+                {
+                    user && !user.error ? 
+                        <div>
+                            <button onClick={logOut}>Log out</button>
+                            <ul>
+                                <li>
+                                    <Link to='/'>Home</Link>
+                                </li>
+                                <li>
+                                    <Link ></Link>
+                                </li>
+                                <li>
+                                    <Link></Link>
+                                </li>
+                            </ul>
+                        </div>  
+                                        
+                        :
+                        <>
+                            <UserForm submit={signUp} header={'Sign up'} />
+                            or
+                            <UserForm submit={logIn} header={'Log in'} />
+                        </>
+                }
+            </nav>
+        </Router>
     )
 }
 
