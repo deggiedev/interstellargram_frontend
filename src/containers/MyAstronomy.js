a459b56fc9ac2fe9dd32d34de0095c6c405bcab8
Jack de Grave <jack.degrave@outlook.com>
2019-12-02 16:53:28 +0000

    render posts after submit without refresh?

--- src/containers/MyAstronomy.js
+++ src/containers/MyAstronomy.js
@@ -4,11 +4,27 @@ import PostForm from '../components/PostForm';
 
 class MyAstronomy extends React.Component {
  
+  //state = {
+  //  posts: []
+  //}
+
+  //getPosts = () => {
+  //  return fetch('http://localhost:3000/posts')
+  //  .then(resp => resp.json())
+  //}
+
+  //componentDidUpdate(prevprops) {
+  //  if (prevprops) {
+  //    this.getPosts()
+  //    .then(posts => this.setState({ posts }))
+  //  }
+  //}
+
     render() {
       return (
           <>
             <PostForm errors={this.props.errors} submit={this.props.submit}/>
-            {this.props.user.posts.map(post => <ImageCard post={post}/>)}
+            {this.props.user.posts.map(post =>  <ImageCard post={post}/>)}
           </>
         );
       }
