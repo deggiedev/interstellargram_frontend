d0509173c695801b18421904ea918a2ffb6bace2
Jack de Grave <jack.degrave@outlook.com>
2019-12-23 21:34:40 +0000

    post form styling v2

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { Form, Button } from 'semantic-ui-react'
 
+
 const PostForm = ({ submit, errors }) => {
 
     const [category, setCategory] = useState('')
@@ -18,10 +19,15 @@ const PostForm = ({ submit, errors }) => {
             {
                 errors && <div style={{ color: 'red' }}>{errors.join(', ')}</div>
             }
-            <span>New post</span>
-            <Form.Input className="Category" placeholder="Category" type="text" name="Category" value={category} onChange={e => setCategory(e.target.value)} />
-            <textarea className="Description" placeholder="Description..." name="description" value={description} onChange={e => setDescription(e.target.value)} />
-            <Form.Input className="Image" placeholder="Image" type="text" name="Image" value={image} onChange={e => setImage(e.target.value)} />
+            <Form.Field/>
+                <input className="ImagePost" placeholder="Image URL" type="text" name="Image" value={image} onChange={e => setImage(e.target.value)} />
+            <Form.Field/>
+            <Form.Field/>
+                <input className="CategoryPost" placeholder="Category" type="text" name="Category" value={category} onChange={e => setCategory(e.target.value)} />
+            <Form.Field/>
+            <Form.Field/>
+                <textarea className="DescriptionPost" placeholder="Description..." name="description" value={description} onChange={e => setDescription(e.target.value)} />
+            <Form.Field/>
             <Button className="SubmitPost" type="submit">Post</Button>
         </Form>
     )
