f1d3550cb2ec2726bed8f95c2a8a773e18ada393
Jack de Grave <jack.degrave@outlook.com>
2020-01-06 21:17:50 +0000

    image card mod

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -4,10 +4,12 @@ import { Image, Card } from 'semantic-ui-react'
 const ImageCard = (props) => {
 
     const src = props.post.image
+    const category = props.post.category
+    const description = props.post.description
 
     return (
        <Card color='grey'>
-            <Image src={src}  />
+            <Image src={src} />
         </Card>
     )
 }
