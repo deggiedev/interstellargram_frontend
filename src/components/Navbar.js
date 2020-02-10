40aee6bf160cf7997a1752a0e57496c40f6492f7
Jack de Grave <jack.degrave@outlook.com>
2020-02-10 14:49:09 +0000

    rebass navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,7 +10,7 @@ import Home from '../containers/Home'
 import MyAstronomy from '../containers/MyAstronomy'
 import CelestialEvents from '../containers/CelestialEvents'
 import { List } from 'semantic-ui-react'
-import { Image, Flex, Button }  from 'rebass'
+import { Image }  from 'rebass'
 import Logo from '../HatchfulExport-All/logo_transparent.png'
 
 const Navbar = ({celestialEvents, posts, user, signUp, logIn, logOut, errors, submit, showUserForm, showUserFormState }) => {
